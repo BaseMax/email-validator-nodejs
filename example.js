dbbe112fca49ac541a51d342933687bd8afe1d0e
Max Base <MaxBaseCode@Gmail.Com>
2023-04-09 14:17:42 +0100

    adding examples for excat

--- example.js
+++ example.js
@@ -10,6 +10,12 @@ console.log(email_validator.has("hi there, here is my email address: maxbasecode
 console.log(email_validator.has("hey please message me to max@asrez.com"));
 console.log(email_validator.has("!!!!"));
 
+// Test exact function
+console.log(email_validator.exact("hi there, here is my email address: maxbasecode@gmail.com please email me otherwise i will contact info@google.com... or at least to info@my.asrez.com or me.you@my.asrez.com"));
+console.log(email_validator.exact("hey please message me to max@asrez.com"));
+console.log(email_validator.exact("max@asrez.com"));
+console.log(email_validator.exact("!!!!"));
+
 // Test includes function
 console.log(email_validator.includes("hey please message me to max@asrez.com", "max@asrez.com"));
 console.log(email_validator.includes("hey please message me to max@asrez.com", "info@google.com"));
