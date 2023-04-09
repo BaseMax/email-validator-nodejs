124388741464bfe16be72736fcc3b2048014bb5b
Max Base <MaxBaseCode@Gmail.Com>
2023-04-09 12:09:01 +0100

    create an example file

---
+++ example.js
@@ -0,0 +1,5 @@
+const email_validator = require("./email-validator");
+
+console.log(email_validator);
+
+console.log(email_validator.match("hi there, here is my email address: maxbasecode@gmail.com please email me otherwise i will contact info@google.com..."));
