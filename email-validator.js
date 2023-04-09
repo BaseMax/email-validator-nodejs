13605b2874a3cbb27919ac31ae4d29145db72308
Max Base <MaxBaseCode@Gmail.Com>
2023-04-09 12:04:12 +0100

    create base functionality to validate email and match and extract all emails inside a text

---
+++ email-validator.js
@@ -0,0 +1,23 @@
+const REGEX = /([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})/g;
+
+// Return an array from all emails address inside the input text
+const match = (text) => {
+    return text.match(REGEX);
+};
+
+// Return true if the input text has at least one email address
+const has = (text) => {
+    return match(text).length > 0;
+};
+
+// Return true if the input text includes the email address
+const includes = (text, email) => {
+    return match(text).includes(email);
+};
+
+// Export the functions
+module.exports = {
+    match,
+    includes,
+    has
+};
