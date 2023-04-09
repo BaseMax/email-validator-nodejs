1b582ebc03052c2835c48eddc1c214000a9583ad
Max Base <MaxBaseCode@Gmail.Com>
2023-04-09 12:12:36 +0100

    adding more examples

--- email-validator.js
+++ email-validator.js
@@ -1,8 +1,4 @@
-// javascript
-// const REGEX = /([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})/g;
-// const REGEX = /([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})(?!\.)/g;
 const REGEX = /([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})\b/g;
-// "info@google.com..."" is an invalid email but we should detect `info@google.com` as an email and not `info@google.com...`.
 
 // Return an array from all emails address inside the input text
 const match = (text) => {
@@ -11,7 +7,7 @@ const match = (text) => {
 
 // Return true if the input text has at least one email address
 const has = (text) => {
-    return match(text).length > 0;
+    return (new Array(match(text)).length) > 0;
 };
 
 // Return true if the input text includes the email address
--- example.js
+++ example.js
@@ -2,4 +2,14 @@ const email_validator = require("./email-validator");
 
 console.log(email_validator);
 
+// Test match function
 console.log(email_validator.match("hi there, here is my email address: maxbasecode@gmail.com please email me otherwise i will contact info@google.com... or at least to info@my.asrez.com or me.you@my.asrez.com"));
+
+// Test has function
+console.log(email_validator.has("hi there, here is my email address: maxbasecode@gmail.com please email me otherwise i will contact info@google.com... or at least to info@my.asrez.com or me.you@my.asrez.com"));
+console.log(email_validator.has("hey please message me to max@asrez.com"));
+console.log(email_validator.has("!!!!"));
+
+// Test includes function
+console.log(email_validator.includes("hey please message me to max@asrez.com", "max@asrez.com"));
+console.log(email_validator.includes("hey please message me to max@asrez.com", "info@google.com"));
